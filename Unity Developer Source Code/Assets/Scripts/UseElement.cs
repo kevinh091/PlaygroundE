0ea91348aaf4f233890e2bfa751946c1c290e100
kevinh091 <qiankaih@buffalo.edu>
2017-11-06 08:02:34 -0500

    Add map graphics
    Add map graphics
    and some others skills

--- Unity Developer Source Code/Assets/Scripts/UseElement.cs
+++ Unity Developer Source Code/Assets/Scripts/UseElement.cs
@@ -305,6 +305,9 @@ public class UseElement : NetworkBehaviour {
                 location.z = -1;
                 CmdAAAD(location);
             }
+			if (num[1] == 2 &&num[4]==2) {  //2 water, 2 earth  // go stealth
+                this.transform.position.z=2;
+            }
             clear();
             CmdButton("Clear", next);
         }
